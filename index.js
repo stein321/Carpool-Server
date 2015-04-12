4c03b9c281bac8976fcfcfc0093a8ad7b23ab33c
pluggednickel <seattlewinter@gmail.com>
2015-04-12 10:23:33 -0700

    Add env file for config vars. Also messing with index to test postgresql

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+
+var express = require('express');
+var app = express();
 var pg = require('pg');
+
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -10,4 +15,8 @@ app.get('/db', function (request, response) {
        { response.send(result.rows); }
     });
   });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 })

c588bfe6289e9606dfc288e171f4f787a5386e1f
pluggednickel <seattlewinter@gmail.com>
2015-06-14 16:34:32 -0700

    Add carpool_user table for testing pg module.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM carpool_user', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
